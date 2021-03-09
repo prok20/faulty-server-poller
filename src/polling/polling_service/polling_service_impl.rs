1964d83ec211084d3d04ab4ad657a68325c4621f
Alexander Prokudin <prok20@inbox.ru>
2021-03-09 06:04:52 +0300

    Implement PollingServiceImpl::get_run()

--- src/polling/polling_service/polling_service_impl.rs
+++ src/polling/polling_service/polling_service_impl.rs
@@ -41,7 +41,7 @@ where
     }
 
     async fn get_run(&self, run_id: RunId) -> ServiceResult<Run> {
-        self.get_run(run_id).await
+        self.run_repo.get_run_by_id(run_id).await
     }
 }
 
@@ -63,6 +63,7 @@ where
 mod should {
     use super::*;
     use crate::polling::background_job_runner::MockBackgroundJobRunner;
+    use crate::polling::dto::RunStatus;
     use crate::polling::run_repository::MockRunRepository;
     use mockall::predicate::eq;
 
@@ -99,34 +100,29 @@ mod should {
         assert_eq!(Ok(StartRunResponseDto { id }), actual_result)
     }
 
-    // #[actix_rt::test]
-    // async fn get_run_correctly() {
-    //     let id = RunId::from_str("b6bdfb95-48a7-471b-ac68-8423f8b987ea").unwrap();
-    //     let request = StartRunRequestDto { seconds: 15 };
-    //
-    //     let run_repo = {
-    //         let mut r = MockRunRepository::new();
-    //         r.expect_generate_run_id()
-    //             .return_const(id);
-    //         r.expect_save_run()
-    //             .with(eq(NewRun { id, seconds: request.seconds }))
-    //             .return_const(ServiceResult::Ok(()));
-    //         r
-    //     };
-    //     let job_runner = {
-    //         let mut j = MockBackgroundJobRunner::new();
-    //         j.expect_try_push_job()
-    //             .with(eq(RunJob {
-    //                 id,
-    //                 duration: std::time::Duration::from_secs(request.seconds),
-    //             }))
-    //             .return_const(ServiceResult::Ok(()));
-    //         j
-    //     };
-    //
-    //     let service = PollingServiceImpl::new(run_repo, job_runner);
-    //
-    //     let actual_result = service.start_run(request).await;
-    //     assert_eq!(Ok(StartRunResponseDto { id }), actual_result)
-    // }
+    #[actix_rt::test]
+    async fn get_run_correctly() {
+        let id = RunId::new_v4();
+        let expected_result = ServiceResult::Ok(Run {
+            id,
+            status: RunStatus::Finished,
+            successful_responses_count: 10,
+            sum: 150,
+        });
+
+        let run_repo = {
+            let mut r = MockRunRepository::new();
+            r.expect_generate_run_id().return_const(id);
+            r.expect_get_run_by_id()
+                .with(eq(id))
+                .return_const(expected_result.clone());
+            r
+        };
+        let job_runner = MockBackgroundJobRunner::new();
+
+        let service = PollingServiceImpl::new(run_repo, job_runner);
+
+        let actual_result = service.get_run(id).await;
+        assert_eq!(expected_result, actual_result)
+    }
 }
